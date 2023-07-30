174ecc603cdf133323cbacc6314913b01d6a60a1
jacqui <gitaujaquiline@gmail.com>
2023-07-30 23:56:40 +0300

    Adding the data model

---
+++ app/src/main/java/com/example/noto/model/Notes.kt
@@ -0,0 +1,14 @@
+package com.example.noto.model
+
+/**
+ * PROJECT NAME: Noto
+ * USER        : jacqui
+ * EMAIL       : gitaujaquiline@gmail.com
+ * DATE        : Jul, 7/30/23
+ * TIME        : 11:53 PM
+ */
+data class Notes(
+    val id: String,
+    val title: String = " ",
+    val description: String = " "
+)
